924a7e27b18f2208c74acf3a4588a70f0883542c
hyf <hyf-0418@outlook.com>
2020-04-25 23:07:05 +0800

    fix bug

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,14 +14,6 @@ axios.defaults.baseURL = '/api'
 axios.defaults.withCredentials = true
 axios.defaults.headers.post['Content-Type'] = "application/json;charset=UTF-8"
 
-let ROOT;
-if (process.env.NODE_ENV === 'development') {
-  ROOT = "/api";                        //开发环境下的代理地址，解决本地跨域
-} else {
-  ROOT = 'http://114.115.246.37:80';    //生产环境下的地址
-}
-axios.defaults.baseURL = ROOT;
-
 // http request 拦截器
 axios.interceptors.request.use(
   config => {
